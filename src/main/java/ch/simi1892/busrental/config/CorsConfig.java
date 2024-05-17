84611bf507c5a7fc964f28b04677d81fc4a0c501
Simon Egli <eglisi1@students.zhaw.ch>
2024-05-17 17:22:22 +0200

    add cors solely locally

--- src/main/java/ch/simi1892/busrental/config/CorsConfig.java
+++ src/main/java/ch/simi1892/busrental/config/CorsConfig.java
@@ -1,21 +1,27 @@
 package ch.simi1892.busrental.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+    @Value("${stage}")
+    private String stage;
+
     /**
      * Configures CORS to allow requests from the frontend
      * @param registry the CorsRegistry
      */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+        if ("dev".equalsIgnoreCase(stage)) {
+            registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:4200")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
+        }
     }
 }
