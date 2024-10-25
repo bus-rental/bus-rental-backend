cefb84e52f11926c04063b4ef69e42314a936455
Simon Egli <simi@simons-macbook-pro-1.home>
2024-10-25 14:01:47 +0200

    add entity bus

--- src/main/java/ch/bus/rental/backend/BackendApplication.java
+++ src/main/java/ch/bus/rental/backend/BackendApplication.java
@@ -2,8 +2,10 @@ package ch.bus.rental.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("ch.bus.rental.entity")
 public class BackendApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/ch/bus/rental/entity/Bus.java
@@ -0,0 +1,14 @@
+package ch.bus.rental.entity;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Bus extends BaseEntity {
+    private String name;
+}
