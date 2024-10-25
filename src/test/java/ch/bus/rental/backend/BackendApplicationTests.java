f4a41f78595bfdf8f2ab992353a6a6c7ae811452
Simon Egli <simi@Simons-MacBook-Pro-2.local>
2024-10-25 08:18:32 +0200

    init project

---
+++ src/test/java/ch/bus/rental/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bus.rental.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
